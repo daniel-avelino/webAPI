219ebaa5992106e9cfc00a63aa0131ad52efa80f
Daniel Avelino <daniel_avelino@outlook.com>
2021-01-25 18:46:23 -0300

    models

---
+++ webAPI/Models/Department.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace webAPI.Models
+{
+    public class Department
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public ICollection<Seller> sellers = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void addSeller(Seller seller)
+        {
+            sellers.Add(seller);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Department department &&
+                   id == department.id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id);
+        }
+
+    }
+}
